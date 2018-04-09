e0980594209b7b8d9f944c7e04ee8c62af915cce
DESKTOP-4H6JAJQ\houn5 <houn524@gmail.com>
2018-04-09 10:33:16 +0900

    javafx 라이브러리 프로젝트 내부에 포함

--- bin/test.java
+++ bin/test.java
@@ -1,10 +1,8 @@
 public class test {
 
     public static void main(String[] args) {
-        Object K = 300;
-        K = 1000;
-        Object N = K;
-        System.out.println(K);
-        System.out.println(N);
+        Object S = 23;
+        S = 33;
+        System.out.println(S);
     }
 }
